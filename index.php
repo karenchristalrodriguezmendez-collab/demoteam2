399190856136b569b72e80c51195805d9342c539
MAYA PEÑATE <168228024+May6103@users.noreply.github.com>
2026-02-04 14:06:32 -0600

    Add index.php file with initial HTML structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenidos</title>
+</head>
+<body>
+    <h1>BIENVENIDOS!!!</h1>
+    <br>
+    <h2>Equipo Ecoskin</h2>
+    <br>
+    <ul>
+        <li><a href="./">modulo de productos</a></li>
+</body>
+</html>
--- login.php
+++ login.php
@@ -15,7 +15,12 @@
         <input type="password" id="password" name="password" required><br><br>
         
         <input type="submit" value="Login">
+        
     </form>
+
+    <form action="index.php" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
 
 </body>
 </html>
